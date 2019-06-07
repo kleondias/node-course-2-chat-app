0cd3ad6ebbba2ee476011a2743244f1c4f8c4b8e
Kleon Dias <kdias76@hotmail.com>
2019-06-07 20:24:44 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,12 +3,6 @@ var socket = io();
 
 socket.on('connect', ()=>{
 	console.log('connected to server');
-
-	socket.emit('createMessage', {
-		from: "Andrew",
-		text:"That works for me."
-	});
-
 });
 
 socket.on('disconnect', ()=>{
