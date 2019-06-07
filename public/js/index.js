4324d44eaec4edba52b1d211a98215757afb9084
Kleon Dias <kdias76@hotmail.com>
2019-06-07 23:31:06 +0300

    Add form for messages and show incoming messages in broswer

--- public/js/index.js
+++ public/js/index.js
@@ -11,10 +11,20 @@ socket.on('disconnect', ()=>{
 
 socket.on('newMessage', (message)=>{
 	console.log('New Message', message);
+	var li = $('<li>');
+	li.text(`${message.from}: ${message.text}`);
+
+	$('#messages').append(li);
 });
  
 
+$('#message-form').on('submit', (e)=>{
+	e.preventDefault();
 
+	socket.emit('createMessage', {
+		from:"User",
+		text: $('[name=message]').val()
+	}, ()=>{
 
-
-
+	});
+});
